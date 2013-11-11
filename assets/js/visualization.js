93a2ef2b0111bca3d7d5a5629f61812616ff6531
Joschka <joschka@kintscher.me>
2013-11-11 08:22:47 +0100

    Animate paths of all particles on canvas

--- assets/js/visualization.js
+++ assets/js/visualization.js
@@ -1,25 +1,74 @@
-$(function() {
+// requestAnim shim layer by Paul Irish
 
-  var canvas = $('#canvas'),
-      ctx    = canvas[0].getContext('2d'),
-      particle;
+(function() {
 
-  ctx.fillStyle = '#000000';
-  ctx.fillRect(0, 0, canvas.width(), canvas.height(), 1, 1);
+  var Particle = function(coords) {
+    this.start = coords[0].frame;
+    this.end = coords[coords.length - 1].frame;
 
-  ctx.strokeStyle = 'rgba(255,255,255,0.7)';
+    this.animate = function(ctx, frame) {
+      if(frame <= this.start || frame > this.end) {
+        return false;
+      }
 
-  for(var i = 0; i < window.particles.length; ++i) {
-    particle = window.particles[i];
-
-    ctx.beginPath();
-    ctx.moveTo(particle[0].x/2, particle[0].y/2);
-
-    for(var j = 1; j < particle.length; ++j) {
-      ctx.lineTo(particle[j].x/2, particle[j].y/2);
+      ctx.beginPath();
+      ctx.moveTo(coords[frame - this.start - 1].x/2, coords[frame - this.start - 1].y/2);
+      ctx.lineTo(coords[frame - this.start].x/2, coords[frame - this.start].y/2);
+      ctx.stroke();
+      return true;
     }
 
-    ctx.stroke();
-  }
+    return this;
+  };
 
-});
+  $(function() {
+
+    var canvas = $('#canvas'),
+        ctx    = canvas[0].getContext('2d'),
+        frame  = 1,
+        elements = [];
+
+    for(var i = 0; i < window.particles.length; ++i) {
+      elements.push(new Particle(window.particles[i]));
+    }
+
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, canvas.width(), canvas.height());
+
+    ctx.strokeStyle = 'rgba(255,255,255,0.6)';
+    ctx.lineWidth = 1;
+
+    function render() {
+      var rendered = false;
+      for(var i = 0; i < elements.length; ++i) {
+        if(elements[i].animate(ctx, frame)) {
+          rendered = true;
+        }
+      }
+      return rendered;
+    }
+
+    setTimeout(function() {
+      var animate = setInterval(function() {
+        if(!render()) {
+          clearInterval(animate);
+        }
+
+        frame++;
+      }, 42);
+    }, 1000);
+
+    return;
+
+    (function animate() {
+      var rid = window.requestAnimationFrame(animate);
+
+      if(!render()) {
+        cancelAnimationFrame(rid);
+      }
+
+      frame++;
+    })();
+  });
+
+})();
