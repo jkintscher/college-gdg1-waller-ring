4658331d59c0e2e30c23e3bb98009989ec306295
Joschka <joschka@kintscher.me>
2013-11-11 06:06:20 +0100

    Output particles as array and directly as JS

--- parse.rb
+++ parse.rb
@@ -5,13 +5,14 @@ require 'json'
 # t, step, pixel_x, pixel_y
 
 id = 1
-coords = {id => []}
+coords = []
 
 Dir.glob(File.join('src/', '*')).each do |file|
+  particle = []
   CSV.foreach(file, :col_sep => "\t") do |row|
     next unless $. > 2
 
-    coords[id] << {
+    particle << {
       :frame => row[1].gsub(/,/, '').to_i,
       :x     => row[2].gsub(/,/, '').to_f,
       :y     => row[3].gsub(/,/, '').to_f,
@@ -20,9 +21,9 @@ Dir.glob(File.join('src/', '*')).each do |file|
   end
 
   id += 1
-  coords[id] = []
+  coords << particle
 end
 
-File.open('coords.json', 'w') do |f|
-  f.write(coords.to_json.to_s.gsub(/\},\{/, "},\n{"))
+File.open('particles.js', 'w') do |f|
+  f.write('var particles = ' + coords.to_json.to_s.gsub(/\},\{/, "},\n{") + ';')
 end
