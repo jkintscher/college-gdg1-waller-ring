f8f4dec13fff0810b92bb2f9b9205cedabcd188e
Joschka <joschka@kintscher.me>
2013-11-11 05:56:22 +0100

    Group coordinates by particle

--- parse.rb
+++ parse.rb
@@ -5,14 +5,13 @@ require 'json'
 # t, step, pixel_x, pixel_y
 
 id = 1
-coords = []
+coords = {id => []}
 
 Dir.glob(File.join('src/', '*')).each do |file|
   CSV.foreach(file, :col_sep => "\t") do |row|
     next unless $. > 2
 
-    coords << {
-      :id    => id,
+    coords[id] << {
       :frame => row[1].gsub(/,/, '').to_i,
       :x     => row[2].gsub(/,/, '').to_f,
       :y     => row[3].gsub(/,/, '').to_f,
@@ -21,6 +20,7 @@ Dir.glob(File.join('src/', '*')).each do |file|
   end
 
   id += 1
+  coords[id] = []
 end
 
 File.open('coords.json', 'w') do |f|
