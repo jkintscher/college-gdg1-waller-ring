97f868e7de2ae2440b1e0b95de2b41a2a57d7636
Joschka <joschka@kintscher.me>
2013-11-11 05:48:07 +0100

    Modify parse script and output JSON instead of CSV

--- parse.rb
+++ parse.rb
@@ -1,32 +1,28 @@
 require 'csv'
 require 'json'
 
-# Headers:
+# Columns:
 # t, step, pixel_x, pixel_y
 
 id = 1
+coords = []
 
-CSV.open('coords.csv', 'wb') do |csv|
-  csv << ['id', 'frame', 'x', 'y', 'type']
+Dir.glob(File.join('src/', '*')).each do |file|
+  CSV.foreach(file, :col_sep => "\t") do |row|
+    next unless $. > 2
 
-  Dir.glob(File.join('src/', '*')).each do |file|
-    CSV.foreach(file, :col_sep => "\t") do |row|
-      next unless $. > 2
-
-      row[0] = id
-      row[1] = row[1].gsub(/,/, '').to_i
-      row[2] = row[2].gsub(/,/, '').to_f
-      row[3] = row[3].gsub(/,/, '').to_f
-
-      if file.match(/person/)
-        row[4] = 'person'
-      else
-        row[4] = 'vehicle'
-      end
-
-      csv << row
-    end
-
-    id += 1
+    coords << {
+      :id    => id,
+      :frame => row[1].gsub(/,/, '').to_i,
+      :x     => row[2].gsub(/,/, '').to_f,
+      :y     => row[3].gsub(/,/, '').to_f,
+      :type  => file.match(/person/) ? 'person' : 'vehicle'
+    }
   end
+
+  id += 1
+end
+
+File.open('coords.json', 'w') do |f|
+  f.write(coords.to_json)
 end
